01c93e999c6252dfc61810b88e344237d00e8847
GabrielMM89 <gdmachadom@eafit.edu.co>
2024-04-18 05:12:24 -0500

    Código actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pensamiento1Component } from "./pensamiento1/pensamiento1.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Pensamiento1Component]
 })
 export class AppComponent {
   title = 'Proyecto-Final';
