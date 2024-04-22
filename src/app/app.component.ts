56d3aef37a9c1d2015ce15979e12bb02f6f84530
GabrielMM89 <gdmachadom@eafit.edu.co>
2024-04-22 06:58:32 -0500

    cambios en relación a fondo y organización de código en pensamiento1 y 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Pensamiento1Component } from "./pensamiento1/pensamiento1.component";
+import { Pensamiento2Component } from './pensamiento2/pensamiento2.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Pensamiento1Component]
+    imports: [RouterOutlet, Pensamiento1Component, Pensamiento2Component]
 })
 export class AppComponent {
   title = 'Proyecto-Final';
