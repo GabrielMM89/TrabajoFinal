1709fea04514e752d066cddea4006a71ef313989
CAMILA0894 <162371735+CAMILA0894@users.noreply.github.com>
2024-04-24 16:24:48 -0500

    componente1 boton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Pensamiento1Component } from './pensamiento1/pensamiento1.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'pensamiento1',
+        component:Pensamiento1Component
     }
 ];
