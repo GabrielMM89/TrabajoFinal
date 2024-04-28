160182f1d6932ab7bd5380218cfd27f0f71038db
CAMILA0894 <162371735+CAMILA0894@users.noreply.github.com>
2024-04-28 16:23:08 -0500

    fix este incendio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-
-import { MenuPlanetasComponent } from './menu-planetas/menu-planetas.component';
-import { Pensamiento1Component } from './pensamiento1/pensamiento1.component';
 import { HomeComponent } from './home/home.component';
+import { Pensamiento1Component } from './pensamiento1/pensamiento1.component';
+import { Pensamiento2Component } from './pensamiento2/pensamiento2.component';
+import { Pensamiento3Component } from './pensamiento3/pensamiento3.component';
+import { Pensamiento5Component } from './pensamiento5/pensamiento5.component';
+import { MenuPlanetasComponent } from './menu-planetas/menu-planetas.component';
 
 export const routes: Routes = [
     {
@@ -10,7 +12,20 @@ export const routes: Routes = [
         component: HomeComponent
     },
     {
-        path: 'planeta',
-        component: Pensamiento1Component
-    }
+        path: 'pensamiento1',
+        component:Pensamiento1Component
+    },
+
+    {
+        path: 'pensamiento2',
+        component:Pensamiento2Component
+    },
+    {
+        path: 'pensamiento3',
+        component:Pensamiento3Component
+    },
+    {
+        path: 'pensamiento5',
+        component:Pensamiento5Component
+    },
 ];
